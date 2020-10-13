3d3027170d01738e8d34873d7ea3883ab2f88dbe
Skye Gao <xg679@nyu.edu>
2020-10-13 11:04:49 +0800

    projectA mapping done

---
+++ projects/experiments/animation-1/script.js
@@ -0,0 +1,5 @@
+// let imgs = document.getElementById("box-1");
+// console.log(imgs.style.animationName);
+function myFunction() {
+  document.getElementById("box-1").style.animationName = "shrink";
+}
--- projects/project_A/script.js
+++ projects/project_A/script.js
@@ -5,25 +5,25 @@ let planning = [
   {
     // source: "images/img00.jpg",
     startingPos: "0",
-    stage1: "400px",
-    stage2: "0px",
-    stage3: "800px",
+    cursorStage1: "10",
+    cursorStage2: "20",
+    cursorStage3: "80",
     speed: "9",
   },
   {
     // source: "images/img01.jpg",
     startingPos: "0",
-    stage1: "500px",
-    stage2: "0px",
-    stage3: "100px",
+    cursorStage1: "10",
+    cursorStage2: "30",
+    cursorStage3: "100",
     speed: "5",
   },
   {
     // source: "images/img01.jpg",
     startingPos: "0",
-    stage1: "500px",
-    stage2: "0px",
-    stage3: "100px",
+    cursorStage1: "20",
+    cursorStage2: "30",
+    cursorStage3: "100",
     speed: "9",
   },
 ]
@@ -40,21 +40,37 @@ for(let i = 0; i < planning.length; i++){
 
 window.addEventListener('scroll', function(){ // on page scroll
   let scrolltop = window.pageYOffset;
-  for(let i = 0; i < imgs.length; i++){
+  let w=window.innerHeight;
+  let percent=scrolltop/(7*w)*100;
+  console.log("div position: "+w*percent+" "+"scrolltop: "+scrolltop+" "+"percentage: "+percent);
 
-    console.log(imgs[i].style.top+" "+planning[i].stage1);
-    // console.log(scrolltop);
-    // console.log(planning[i].stage1);
-    // imgs[1].style.top = Number(scrolltop)/2-Number(planning[1].startingPos) + "px";
-    // imgs[2].style.top = Number(scrolltop)-Number(planning[2].startingPos) + "px";
+  let position=map(percent,0, 100, 50, 100);
+  console.log(position);
 
-    if (imgs[i].style.top>=planning[i].stage1){
-      imgs[i].style.top=-Number(scrolltop)+Number(planning[i].startingPos) + "px";
-    }else{
-      imgs[i].style.top = Number(scrolltop)+Number(planning[i].startingPos) + "px";
-    }
+  //mapping percentage to certain range
+  function map(percent,in_min, in_max, out_min, out_max) {
+    return (percent - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
   }
 
+  for(let i = 0; i < imgs.length; i++){
+    let cursorStage1=planning[i].cursorStage1;
+    let cursorStage2=planning[i].cursorStage2;
+    let cursorStage3=planning[i].cursorStage3;
+    // console.log(planning[i].stage1);
+    console.log(cursorStage1, cursorStage2);
+    if (percent>=0&&percent<cursorStage1){
+      imgs[i].style.top = map(percent,0,cursorStage1,0,200)+"px";
+    }else if (percent>=cursorStage1 && percent<cursorStage2){
+      imgs[i].style.top = map(percent,cursorStage1,cursorStage2,200,0)+"px";
+    }else if(percent>=cursorStage2 && percent<cursorStage3){
+      imgs[i].style.top = map(percent,cursorStage2,cursorStage3,0,500)+"px";
+    }
+    // else{
+    //   imgs[1].style.top = w*percent+"px";
+    //   imgs[0].style.top = w*percent+"px";
+    // }
+
+  }
 }, false)
 
 //-------------------------hard code examples in window.addEventListener---------------
